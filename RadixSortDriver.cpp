cab9ea0ffee19f035882c7467765274a9351c8c0
idgregory42 <idgregory42@students.tntech.edu>
2016-10-26 17:26:23 -0500

    final

--- RadixSortDriver.cpp
+++ RadixSortDriver.cpp
@@ -28,20 +28,21 @@ int sortChoice()
 	//I changed Keyboard.cpp and .h because if you type in a characted for getValidatedInt, it crashes
 	Keyboard* kb = Keyboard::getKeyboard();
 	
+	String* end_line = new String("\n");
+	
 	String* min = new String("1");
 	String* max = new String("3");
-	String* second = new String("(1)Radix Sort Ascending   --  \r\n  -- (2)Radix Sort Descending  --  \r\n  -- (3)Quit                   ");
-	String* first = new String("\r\n  -------------------------------\r\n  --      Radix Sort Menu      --\r\n  -------------------------------\r\n  -- ");
+
 	
+
+	int sort_choice = kb->getValidatedInt("\r\n  -------------------------------\r\n  --      Radix Sort Menu      --\r\n  -------------------------------\r\n  -- (1)Radix Sort Ascending   --  \r\n  -- (2)Radix Sort Descending  --  \r\n  -- (3)Quit                   --  \r\n  ------------------------------- \r\n  Menu Choice: ", min, max);
 	
-	first->displayString();
-	second->displayString();
-	int sort_choice = kb->getValidatedInt("--  \r\n  ------------------------------- \r\n  Menu Choice: ", min, max);
+	end_line->displayString();
 	
 	delete min;
 	delete max;
-	delete first;
-	delete second;
+	delete end_line;
+	
 	return sort_choice;
 }
 
@@ -104,7 +105,7 @@ void displayDem(CD** c_ds, int size)
 int main()
 {
    ListArray<CD>* list = CD::readCDs("cds.txt");
-   int size = list->size();  
+   int size = list->size();
    int num_chars = 0;          //for max num of chars
    bool ascending = true;      //boolean for the radixSort parameters
    
@@ -115,56 +116,68 @@ int main()
    String* d = new String("Descending Sort:\n");
    
    CD** cds = new CD*[size];
-   ListArrayIterator<CD>* iter = list->iterator();
-   int count = 0;
-   while(iter->hasNext())
-   {
-      CD* cd = iter->next();
-	  cds[count] = cd;
-	  
-		String* cd_t = cds[count]->getKey();
-		int str_size = cd_t->length();
-		if(str_size > num_chars) num_chars = str_size;
-		
-	  count++;
-   }
-   delete iter;
-   
 	int choice = 0;
 	bool valT = false;
 	int i = 0;
-	CD** c_ds;
-	
+
+	CD** cds_a;
+
    do
    {
 	   choice = sortChoice();
 	   
+	   if(choice != 3)
+	   {
+		    
+			ListArrayIterator<CD>* iter = list->iterator();
+			int count = 0;
+			while(iter->hasNext())
+			{
+				CD* cd = iter->next();
+				cds[count] = cd;
+	  
+				String* cd_t = cds[count]->getKey();
+				int str_size = cd_t->length();
+				if(str_size > num_chars) num_chars = str_size;
+		
+				count++;
+			}
+			delete iter;		   
+	   }
+	   
 	   if(choice == 1)
 	   {
-			c_ds = RadixSort<CD>::radixSort(cds, size, num_chars, true, &CD::getRadixChar);  //sorts dem cds
-			displayDem(c_ds, size);
+			
+			cds_a = RadixSort<CD>::radixSort(cds, size, num_chars, true, &CD::getRadixChar);  //sorts dem cds
+			displayDem(cds_a, size);
 			a->displayString();
+
+
 	   }
 	   else if(choice == 2)
 	   {
-			c_ds = RadixSort<CD>::radixSort(cds, size, num_chars, false, &CD::getRadixChar);  //sorts dem cds
-			displayDem(c_ds, size);
+			cds_a = RadixSort<CD>::radixSort(cds, size, num_chars, false, &CD::getRadixChar);  //sorts dem cds
+			displayDem(cds_a, size);
 			d->displayString();
 
 	   }
 	   else
 	   {
 		   i = surePrompt();
+		   cds_a = new CD*[size];
 		   if(i == 3) break;
+		   
 	   }
-	   
 		
    }while(!valT);
    
 
 
 
-   delete[] cds;
+
+   delete[] cds; 
+	delete[] cds_a;
+
    deleteCDs(list);
    delete list;
    delete donezo;
